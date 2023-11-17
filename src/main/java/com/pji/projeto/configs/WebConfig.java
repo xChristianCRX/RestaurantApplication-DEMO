d4f52c727124c1cd3395b1ea01897a54f235e88c
Christian Ricci <riccichristian338@gmail.com>
2023-11-17 12:01:44 -0300

    Feat: CORS modified

--- src/main/java/com/pji/projeto/configs/WebConfig.java
+++ src/main/java/com/pji/projeto/configs/WebConfig.java
@@ -1,18 +1,24 @@
 package com.pji.projeto.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig  {
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
